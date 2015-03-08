4fdc588cf343094d12aa7b48b56185b7267e8c5b
axton21 <axton_n@yahoo.com>
2015-03-08 16:36:17 -0400

    ʕ̡̢̡*✪௰✪ૢʔ̢̡̢

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,3 @@
-# Create your HauntedHouse class hereclass HauntedHouse < ActiveRecord::Base
+class HauntedHouse < ActiveRecord::Base
+
+end
--- db/migrations/003_create_haunted_houses.rb
+++ db/migrations/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name, :location, :theme, :description
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date, :closing_date
+
+      t.timestamps true
+    end
+  end
+end
